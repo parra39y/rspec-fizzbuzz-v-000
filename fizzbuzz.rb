76457561a490067c88013931863f07e5d3395f62
Yesenia Parra <parra22y@mtholyoke.edu>
2018-04-08 21:00:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,22 @@
 def fizzbuzz (number)
-  if number%3==0 #if the number int is divisible by 3
-    return "Fizz" #Go fizz
-end
-  if number%5==0 #if the number int is divisible by 5
-  return "Buzz" #Go Buzz
-end
-  if (number%3==0 && number%5==0) #if the number int is divisible by 3 and 5
-  return "FizzBuzz" #Go FizzBuzz
-end
+  #if number%3==0 #if the number int is divisible by 3
+    #return "Fizz" #Go fizz
+#end
+  #if number%5==0 #if the number int is divisible by 5
+  #return "Buzz" #Go Buzz
+#end
+#  if (number%3==0 && number%5==0) #if the number int is divisible by 3 and 5
+#  return "FizzBuzz" #Go FizzBuzz
+#end
 if (number%3!=0 && number%5!=0)
   return nil
+elsif (number%3==0 && number%5==0) #if the number int is divisible by 3 and 5
+ return "FizzBuzz" #Go FizzBuzz
+elsif number%3==0 #if the number int is divisible by 3
+  return "Fizz" #Go fizz
+elsif number%5==0 #if the number int is divisible by 5
+return "Buzz" #Go Buzz
+
 end
 end
 
