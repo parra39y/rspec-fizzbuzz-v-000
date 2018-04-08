2c38c97a85122d5238b82b3ed07de157df10e0de
Yesenia Parra <parra22y@mtholyoke.edu>
2018-04-08 20:37:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
 def fizzbuzz (number)
   if number%3==0 #if the number int is divisible by 3
-    puts "Fizz" #Go fizz
+    return "Fizz" #Go fizz
 end
   if number%5==0 #if the number int is divisible by 5
-  puts "Buzz" #Go Buzz
+  return "Buzz" #Go Buzz
 end
   if number%3==0 and number%5==0 #if the number int is divisible by 3 and 5
-  puts "FizzBuzz" #Go FizzBuzz
+  return "FizzBuzz" #Go FizzBuzz
 end
 if number%3!=0 and number%5!=0
-  puts "nil"
+  return "nil"
 end
 end
 
