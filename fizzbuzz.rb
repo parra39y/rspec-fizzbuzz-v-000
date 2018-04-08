937edeff2f6a06c1f717afa43b53f7388748edc6
Yesenia Parra <parra22y@mtholyoke.edu>
2018-04-08 20:46:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,7 +8,7 @@ end
   if number%3==0 && number%5==0 #if the number int is divisible by 3 and 5
   return "FizzBuzz" #Go FizzBuzz
 end
-else number%3!=0 && number%5!=0
+if number%3!=0 && number%5!=0
   return "nil"
 end
 end
